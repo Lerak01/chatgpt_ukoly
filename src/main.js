7bcc7606b472245dd66889a4c3eb90f6b82e2411
marte <karel-m@seznam.cz>
2025-01-11 18:31:11 +0100

    vylepseni ukolu o otazku a funkci

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-for (let i = 0; i <= 10; i++){
-    if (i%2===0) {
-        console.log(i + " " + "je sudé");
-    }
-    else{
-        console.log(i+ " " + "je liché")
+let number = prompt("Zadejte číslo k určení: ")
+function add_number(number){
+    for (let i = 1; i <= number; i++){
+        if (i%2===0) {
+            console.log(i + " " + "je sudé");
         }
 
+        else{
+            console.log(i+ " " + "je liché")
+        }
+
+    }
+
 }
+
+add_number(number)
